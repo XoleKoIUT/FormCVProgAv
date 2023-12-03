f5aea6c2fde00c16fe63dae30e43970b6fb4f27a
XoleKo <xolekoiut76@gmail.com>
2023-12-03 20:22:46 +0100

    Page à propos de moi //DONE

--- app/Views/CV_Content/aboutMe.php
+++ app/Views/CV_Content/aboutMe.php
@@ -1,20 +1,21 @@
 <div class="container-fluid mt-4">
     <div class="row align-items-center justify-content-center">
-        <div class="col-md-6 text-justify">
-            <h1 class="py-3 text-center text-primary">A propos de moi</h1>
-                <p>
-                    Etudiant en BUT Informatique au Havre, je suis passionné par
-                    le développement informatique et j'ai un fort intérêt pour les nouvelles technologies.
-                    Je maîtrise les langages de programmation Java, PhP, Html, JQuery, Javascript, CSS, SQL et j'en passe.
-                    J'ai également une bonne connaissance des bases de données et des systèmes d'exploitation.
-                    Au cours de ma formation, j'ai réalisé plusieurs projets concrets,
-                    notamment lors de mon stage chez Enedis où j'ai pu développer une application WEB, le "Bilan Social NAT".
-                    Je suis une personne dynamique, motivée et adaptable.
-                    J'ai un bon sens du relationnel et je suis capable de travailler en équipe.
-                </p>
-
-                    Après l'obtention de mon diplôme, je souhaiterais être embauché chez Enedis ( entreprise dans laquelle
-                    j'ai effectué mon stage de 2nd année ainsi que mon alternance en 3ème année ) en tant que développeur WEB.
+        <div class="col-md-6 text-center text-md-left">
+            <h1 class="py-3 text-primary">À propos de moi</h1>
+            <p>
+                En tant qu'étudiant en BUT Informatique au Havre, ma passion pour le développement informatique
+                et mon intérêt pour les nouvelles technologies me motivent constamment.
+                Je maîtrise divers langages de programmation tels que Java, PHP, HTML, JQuery, JavaScript, CSS et SQL.
+                Mes compétences s'étendent également aux bases de données et aux systèmes d'exploitation.
+                Mon parcours académique inclut la réalisation de projets concrets, notamment mon stage chez Enedis où
+                j'ai développé une application WEB, le "Bilan Social NAT".
+                Dynamique, motivé et adaptable, je possède un bon sens du relationnel et je suis capable de travailler en équipe.
+            </p>
+            <p>
+                Après l'obtention de mon diplôme, mon objectif professionnel est de rejoindre Enedis en tant que développeur WEB.
+                Mon expérience précédente au sein de l'entreprise pendant mon stage de 2e année et mon alternance en 3e année
+                renforce ma conviction que c'est l'endroit idéal pour continuer à développer mes compétences et contribuer au domaine du développement web.
+            </p>
         </div>
         <div class="col-md-6 text-center">
             <img src="/assets/images/AboutMe.png" alt="Photo_CV" width="200" height="200" class="img-fluid">
