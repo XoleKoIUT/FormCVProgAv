bc34c8bf11c623992703b7e80d90b1ae987d9f71
XoleKo <xolekoiut76@gmail.com>
2023-12-03 22:30:23 +0100

    2/3 modifs

--- app/Views/CV_Content/career.php
+++ app/Views/CV_Content/career.php
@@ -1,6 +1,19 @@
-<div class="container position-relative">
-    <img src="/assets/images/parcours.png" alt="parcours">
-    <div class="position-absolute top-0 start-0">
-        <p>Contenu au-dessus de l'image</p>
+<div class="container-fluid parcours-container position-relative">
+    <img src="/assets/images/parcours.png" style="width:75%;" class="parcours-image mx-auto d-block">
+    <div class="parcours-text">
+        <h1 class="text-primary">Mon Parcours</h1>
+        <p>
+            J'ai débuté mon cursus scolaire au Collège Henry Matisse.
+            Par la suite, j'ai poursuivi mes études au Lycée Ferdinand Buisson,
+            où j'ai obtenu un BAC STI2D avec Mention Bien,
+            avec une spécialisation en Option SIN (Systèmes d'Information et Numérique).
+        </p>
+        <p>
+            Mon parcours académique m'a conduit à l'IUT du Havre,
+            où j'ai entamé un cursus en BUT Informatique. Parallèlement à mes études,
+            j'ai eu l'opportunité de réaliser un stage, des missions en intérim,
+            et une alternance chez Enedis à Rouen, enrichissant ainsi ma formation académique par
+            une expérience professionnelle concrète.
+        </p>
     </div>
 </div>
--- app/Views/CV_Content/softSkills.php
+++ app/Views/CV_Content/softSkills.php
@@ -1,7 +1,19 @@
-<div class="container-fluid mt-4">
-    <h1 class="text-center text-primary py-3">Soft Skills</h1>
-    <div class="position-relative bg-secondary p-4 rounded-top">
-        <p class="text-white">Votre texte ici</p>
-        <div class="position-absolute top-0 start-50 translate-middle-x border-width-20 border-style-solid border-transparent border-bottom border-secondary"></div>
+<div class="container-fluid soft-skills-container">
+    <h1 class="text-center text-primary pb-3">Soft Skills</h1>
+    <div class="soft-skills-box">
+        <p class="text-white">
+            En tant qu'étudiant en 3ème année de BUT Informatique,
+            je développe un ensemble de compétences "soft skills" qui complètent mon parcours académique.
+            Ces compétences incluent :
+        </p>
+        <ul class="text-white">
+            <li>Communication efficace</li>
+            <li>Résolution créative de problèmes</li>
+            <li>Esprit d'équipe et collaboration</li>
+            <li>Adaptabilité et flexibilité</li>
+            <li>Autonomie</li>
+            <li>Persévérance</li>
+        </ul>
+        <div class="arrow-down"></div>
     </div>
 </div>
